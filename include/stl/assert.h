430727773bdd6641f6834c60470b0076555ad04f
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-04-29 23:22:30 -0500

    feat(assert): add assertion utilities and macros

---
+++ include/stl/assert.h
@@ -0,0 +1,36 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#include <map>
+#include "stl/common.h"
+#include "stl/exceptions.h"
+
+const std::map<char*, int> tests;
+
+namespace stl {
+    class assertion_exception : public exception
+    {
+        const char* what() const noexcept {
+            return "assertion exception";
+        }
+    };
+    inline void assert(bool condition, const char *message, int line = __LINE__, const char* file = __FILE__)
+    {
+        if (condition)
+        {
+            printf("[assertion] %s: %s:%d\n", "passed", getFileName(file), line);
+        }
+        else
+        {
+            printf("[assertion] %s: %s:%d\n", message, getFileName(file), line);
+        }
+        
+    }
+};
+
+#define ASSERT_EQUAL(a, b) stl::assert(a == b, "expected equal, got not equal", __LINE__, __FILE__);
+#define ASSERT_NOT_EQUAL(a, b) stl::assert(a != b, "expected not equal, got equal", __LINE__, __FILE__);
+#define ASSERT_NULL(a) stl::assert(a == nullptr, "expected nullptr, got defined", __LINE__, __FILE__);
+#define ASSERT_THROWS(function) try { function() } catch () { stl::assert(false, "", __LINE__, __FILE__) }
+
+#endif
