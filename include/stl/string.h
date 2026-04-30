fbab7d78b62e4622b899a1767492f07a59811ac1
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-04-29 23:22:20 -0500

    feat(string): add strlen implementation

---
+++ include/stl/string.h
@@ -0,0 +1,20 @@
+#ifndef STRING_H
+#define STRING_H
+#include "types.h"
+
+namespace stl
+{
+    inline stl::size_t strlen(const char* str) noexcept
+    {
+        stl::size_t count = 0;
+        char i = str[count];
+        while(i != '\0')
+        {
+            count++;
+            i = str[count];
+        }
+        return count;
+    }
+}; // namespace
+
+#endif
