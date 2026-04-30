bcdc983f719c9deec52c7d4071545fffba89832d
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-04-29 19:07:32 -0500

    feat: added types

---
+++ include/stl/types.h
@@ -0,0 +1,17 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+namespace stl
+{
+    // 64 bit size_t
+    #if defined(__x86_64__) || defined(_M_X64) || defined(__aarch64__) || defined(__LP64__) || defined(_WIN64)
+    using size_t = unsigned long long;
+    #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    // 32 bit size_t
+    using size_t = unsigned int;
+    #else
+    // default size is 32 bit architecture size.
+    using size_t = unsigned int;
+    #endif
+};
+#endif
