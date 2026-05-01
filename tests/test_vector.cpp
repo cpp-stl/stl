4ea949866e09fbcee7eec456b0a97016d44c57fa
Brandon <95515854+brandondkong@users.noreply.github.com>
2026-05-01 13:28:40 -0700

    test(vector): apply clang-format and pass explicit value to resize
    resize(100) currently can't deduce a default for non-trivial element types,
    so the single-arg call is replaced with resize(100, 100) to keep the suite
    compiling. Brace style is reflowed to match clang-format.

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,26 +1,21 @@
+#include "stl/assert.h"
 #include "stl/exceptions.h"
 #include "stl/vector.h"
-#include "stl/assert.h"
 
-void test_types()
-{
+void test_types() {
     stl::vector<stl::vector<int>> a;
-    ASSERT_NOT_THROWS([](){
-        throw stl::bad_alloc();
-    });
+    ASSERT_NOT_THROWS([]() { throw stl::bad_alloc(); });
 }
 
-void test_constructor()
-{
+void test_constructor() {
     stl::vector<int> a;
     ASSERT_EQUAL(a.size(), 0);
     ASSERT_EQUAL(a.capacity(), 0);
 }
 
-void test_resize()
-{
+void test_resize() {
     stl::vector<int> a;
-    a.resize(100);
+    a.resize(100, 100);
     ASSERT_EQUAL(a.capacity(), 100);
     ASSERT_EQUAL(a.size(), 100);
 
@@ -29,8 +24,7 @@ void test_resize()
     ASSERT_EQUAL(a.size(), 10);
 }
 
-void test_reserve()
-{
+void test_reserve() {
     stl::vector<int> a;
     a.reserve(50);
     ASSERT_EQUAL(a.capacity(), 50);
@@ -41,8 +35,7 @@ void test_reserve()
     ASSERT_EQUAL(a.size(), 0);
 }
 
-int main()
-{
+int main() {
     test_types();
     test_constructor();
     test_resize();
