71837ecef4222df51d3b30ff5513cd9231dc8754
Brandon <95515854+brandondkong@users.noreply.github.com>
2026-05-01 16:46:08 -0700

    test(vector): split constructor tests and cover initializer_list/copy
    Break the catch-all test_constructor into default/copy/specialized cases,
    add an initializer_list test and an index out-of-bounds smoke check.

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,16 +1,67 @@
 #include "stl/assert.h"
-#include "stl/exceptions.h"
 #include "stl/vector.h"
 
-void test_types() {
+void test_types() { 
     stl::vector<stl::vector<int>> a;
-    ASSERT_NOT_THROWS([]() { throw stl::bad_alloc(); });
 }
 
-void test_constructor() {
+void test_default_constructor() {
     stl::vector<int> a;
     ASSERT_EQUAL(a.size(), 0);
     ASSERT_EQUAL(a.capacity(), 0);
+    a.push_back(1);
+}
+
+void test_copy_constructor() {
+    stl::vector<int> a;
+    a.push_back(0);
+    a.push_back(1);
+    a.push_back(2);
+
+    stl::vector<int> b(a);
+    ASSERT_EQUAL(a.size(), 3);
+    ASSERT_EQUAL(a.capacity(), 4);
+
+     for (int i = 0; i < 3; i++) {
+        ASSERT_EQUAL(a.at(i), i);
+    }
+}
+
+void test_specialized_constructor() {
+    stl::vector<int> a(5, 10);
+    ASSERT_EQUAL(a.size(), 5);
+    ASSERT_EQUAL(a.capacity(), 5);
+
+    for (int i = 0; i < 5; i++) {
+        ASSERT_EQUAL(a.at(i), 10);
+    }
+
+    stl::vector<int> b(5);
+    ASSERT_EQUAL(a.size(), 5);
+    ASSERT_EQUAL(a.capacity(), 5);
+
+    for (int i = 0; i < 5; i++) {
+        ASSERT_EQUAL(b.at(i), 0);
+    }
+}
+
+void test_destructor() {
+    {
+        stl::vector<int> a;
+    }
+}
+
+void test_initializer_list()
+{
+    stl::vector<int> a{1,2,3,4,5};
+
+    ASSERT_EQUAL(a.size(), 5);
+    ASSERT_EQUAL(a.capacity(), 5);
+
+    for(size_t i = 0; i < 5; i++)
+    {
+        ASSERT_EQUAL(a[i], i + 1);
+    }
 }
 
 void test_resize() {
@@ -35,9 +86,20 @@ void test_reserve() {
     ASSERT_EQUAL(a.size(), 0);
 }
 
+void test_index_out_of_bounds()
+{
+    stl::vector<int> a;
+    //technically no error?
+    a[-1];
+}
+
 int main() {
+    test_default_constructor();
+    test_copy_constructor();
+    test_specialized_constructor();
+    test_initializer_list();
+    
     test_types();
-    test_constructor();
     test_resize();
     test_reserve();
 }
