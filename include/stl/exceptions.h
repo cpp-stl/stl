80ae07858ae0e22bb006c00afe3d76010c69e62a
Brandon <95515854+brandondkong@users.noreply.github.com>
2026-05-01 21:45:13 -0700

    feat(exceptions): add null_pointer_exception and assertion_exception

--- include/stl/exceptions.h
+++ include/stl/exceptions.h
@@ -4,24 +4,33 @@
 namespace stl {
 class exception {
    public:
-    exception() noexcept = default;
-    exception(const exception &) noexcept = default;
-    exception &operator=(const exception &) noexcept = default;
-    ~exception() noexcept = default;
+    exception() noexcept(true) = default;
+    exception(const exception &) noexcept(true) = default;
+    exception &operator=(const exception &) noexcept(true) = default;
+    ~exception() noexcept(true) = default;
 
-    virtual const char *what() const noexcept = 0;
+    virtual const char *what() const noexcept(true) = 0;
 };
 
 class out_of_bounds : public exception {
    public:
-    const char *what() const noexcept override { return "out of bounds"; }
+    const char *what() const noexcept(true) override { return "out of bounds"; }
 };
 
 class bad_alloc : public exception {
    public:
-    const char *what() const noexcept override { return "bad allocation"; }
+    const char *what() const noexcept(true) override { return "bad allocation"; }
 };
 
+class null_pointer_exception: public exception {
+    public:
+     const char *what() const noexcept(true) override {return "attempted to dereference null ptr"; }
+};
+
+class assertion_exception : public exception {
+    public:
+     const char *what() const noexcept { return "assertion exception"; }
+    };
 };  // namespace stl
 
 #endif
