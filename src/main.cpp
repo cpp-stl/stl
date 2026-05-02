05de9af06acb6d17d1e92225daa2eb1d3c1214f0
Brandon <95515854+brandondkong@users.noreply.github.com>
2026-05-01 21:45:26 -0700

    chore(src): swap sandbox main to std::vector iterator demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
-#include "stl/vector.h"
 #include "vector"
 #include <iostream>
+#include <vector>
 int main()
 {
-    stl::vector<int> a{1,2,3,4};
-    stl::vector<int>::iterator begin = a.begin();
-    stl::vector<int>::iterator end = a.end();
-    for(auto i = begin; i != end; i++)
-    {
-        int val = *i;
-        std::cout << val << std::endl;
-    }
+    std::vector<int> a;
+    a.push_back(1);
+    a.clear();
+    std::cout << (a.begin().base() == nullptr ? "true" : "false") << std::endl;
 }
