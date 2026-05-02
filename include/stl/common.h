a7209b42f44d1aadeb14c316cbec7dce1cd58652
Brandon <95515854+brandondkong@users.noreply.github.com>
2026-05-01 21:45:08 -0700

    refactor(common): swap getFileName helper for platform/arch macros and fn-pointer alias

--- include/stl/common.h
+++ include/stl/common.h
@@ -1,31 +1,15 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include "stl/types.h"
-#include "string.h"
+template <typename T, typename ...PARAMS>
+using _FUNCTION_POINTER = T(*)(PARAMS...);
 
-inline void getFileName(const char *file_path, char *&buffer) {
-    stl::size_t len = stl::strlen(file_path);
-#if defined(_WIN64) || defined(_M_X64)
-    char slash = '\\';
-#else
-    char slash = '/';
-#endif
-    stl::size_t pos = len;
-    char start = file_path[pos];
-    while (start != slash) {
-        pos--;
-        start = file_path[pos];
-    }
-    pos = pos + 1;
+#define WINDOWS defined(_WIN64) || defined(_M_X64)
+#define _64BIT defined(__x86_64__) || defined(_M_X64) || defined(__aarch64__) || defined(__LP64__) || \
+    defined(_WIN64)
+#define _32BIT defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
 
-    buffer = new char[len - pos + 1];
-    stl::size_t incr = 0;
-    while (file_path[pos + incr] != '\0') {
-        buffer[incr] = file_path[pos + incr];
-        incr++;
-    }
-    buffer[incr] = '\0';
-}
+#define POINTERDIFF(R, L) stl::ptrdiff_t diff() {return (static_cast<int*>(R) - static_cast<int*>(L));}
+// RIP to getFileName(), my favorite method I did first try - Joseph Alemu
 
 #endif
